b8da8d7529f19c68d427ff3bc2c0aec1af60da61
proku <proku@M15-I5W301>
2024-02-01 18:55:04 +0300

    Добавьте файлы проекта.

---
+++ products/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace products
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBoxFilter = new System.Windows.Forms.ComboBox();
+            this.comboBoxSort = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxArticle = new System.Windows.Forms.TextBox();
+            this.textBoxCost = new System.Windows.Forms.TextBox();
+            this.textBoxType = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSearch.Location = new System.Drawing.Point(8, 18);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(174, 28);
+            this.textBoxSearch.TabIndex = 0;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Tan;
+            this.panel1.Controls.Add(this.comboBoxFilter);
+            this.panel1.Controls.Add(this.comboBoxSort);
+            this.panel1.Controls.Add(this.textBoxSearch);
+            this.panel1.Location = new System.Drawing.Point(64, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(749, 61);
+            this.panel1.TabIndex = 1;
+            // 
+            // comboBoxFilter
+            // 
+            this.comboBoxFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxFilter.FormattingEnabled = true;
+            this.comboBoxFilter.Location = new System.Drawing.Point(477, 18);
+            this.comboBoxFilter.Name = "comboBoxFilter";
+            this.comboBoxFilter.Size = new System.Drawing.Size(247, 30);
+            this.comboBoxFilter.TabIndex = 2;
+            this.comboBoxFilter.SelectedIndexChanged += new System.EventHandler(this.comboBoxFilter_SelectedIndexChanged);
+            // 
+            // comboBoxSort
+            // 
+            this.comboBoxSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxSort.FormattingEnabled = true;
+            this.comboBoxSort.Location = new System.Drawing.Point(285, 18);
+            this.comboBoxSort.Name = "comboBoxSort";
+            this.comboBoxSort.Size = new System.Drawing.Size(186, 30);
+            this.comboBoxSort.TabIndex = 1;
+            this.comboBoxSort.SelectedIndexChanged += new System.EventHandler(this.comboBoxSort_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Silver;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(160, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(598, 226);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(467, 343);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(266, 226);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(55, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 61);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Создать штрихкод";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxName.Location = new System.Drawing.Point(21, 46);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(223, 28);
+            this.textBoxName.TabIndex = 5;
+            // 
+            // textBoxArticle
+            // 
+            this.textBoxArticle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxArticle.Location = new System.Drawing.Point(21, 12);
+            this.textBoxArticle.Name = "textBoxArticle";
+            this.textBoxArticle.Size = new System.Drawing.Size(223, 28);
+            this.textBoxArticle.TabIndex = 6;
+            // 
+            // textBoxCost
+            // 
+            this.textBoxCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxCost.Location = new System.Drawing.Point(21, 80);
+            this.textBoxCost.Name = "textBoxCost";
+            this.textBoxCost.Size = new System.Drawing.Size(223, 28);
+            this.textBoxCost.TabIndex = 7;
+            // 
+            // textBoxType
+            // 
+            this.textBoxType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxType.Location = new System.Drawing.Point(21, 114);
+            this.textBoxType.Name = "textBoxType";
+            this.textBoxType.Size = new System.Drawing.Size(223, 28);
+            this.textBoxType.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Tan;
+            this.panel2.Controls.Add(this.textBoxType);
+            this.panel2.Controls.Add(this.textBoxCost);
+            this.panel2.Controls.Add(this.textBoxArticle);
+            this.panel2.Controls.Add(this.textBoxName);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(180, 346);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(266, 223);
+            this.panel2.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(697, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(21, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(733, 311);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(875, 585);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBoxFilter;
+        private System.Windows.Forms.ComboBox comboBoxSort;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxArticle;
+        private System.Windows.Forms.TextBox textBoxCost;
+        private System.Windows.Forms.TextBox textBoxType;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ products/Form1.cs
@@ -0,0 +1,333 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace products
+{
+    public partial class Form1 : Form
+    {
+        Database database = new Database();
+        string[] prod_types = { "Все типы", "Оружие", "Украшение", "Музыкальный инструмент" };
+        string[] prod_cost = { "По возрастанию", "По убыванию" };
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (string types in prod_types)
+            {
+                comboBoxFilter.Items.Add(types);
+            }
+
+            foreach (string costs in prod_cost)
+            {
+                comboBoxSort.Items.Add(costs);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateColumns(dataGridView1);
+            RefreshDataGrid(dataGridView1);
+        }
+
+        private void CreateColumns(DataGridView dataGridView1)
+        {
+            //dataGridView1.Columns.Add("picture", "Изображение");
+            dataGridView1.Columns.Add("article", "Артикул");
+            dataGridView1.Columns.Add("prod_name", "Наименование продукта");
+            dataGridView1.Columns.Add("cost", "Стоимость");
+            dataGridView1.Columns.Add("type_prod", "Категория");
+        }
+
+        public void ReadSingleRow(DataGridView dgw, IDataRecord record)
+        {
+            dgw.Rows.Add(record.GetInt64(0), record.GetString(1), record.GetInt32(2), record.GetString(3));
+        }
+
+        public void RefreshDataGrid(DataGridView dgw)
+        {
+            dgw.Rows.Clear();
+            string queryString = $"select * from items";
+
+            NpgsqlCommand command = new NpgsqlCommand(queryString, database.GetConnection());
+
+            database.OpenConnection();
+
+            NpgsqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ReadSingleRow(dgw, reader);
+            }
+            reader.Close();
+            database.CloseConnection();
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            Search(dataGridView1);
+        }
+
+        private void Search(DataGridView dgw)
+        {
+            dgw.Rows.Clear();
+
+            string query = $"select * from items where concat (prod_name, cost) like '%" + textBoxSearch.Text + "%'";
+            NpgsqlCommand comm = new NpgsqlCommand(query, database.GetConnection());
+
+            database.OpenConnection();
+
+            NpgsqlDataReader read = comm.ExecuteReader();
+
+            while (read.Read())
+            {
+                ReadSingleRow(dgw, read);
+            }
+
+            read.Close();
+            database.CloseConnection();
+        }
+
+        private void comboBoxSort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxSort.SelectedItem == "По возрастанию")
+            {
+                dataGridView1.Sort(dataGridView1.Columns[2], ListSortDirection.Ascending);
+            }
+
+            if (comboBoxSort.SelectedItem == "По убыванию")
+            {
+                dataGridView1.Sort(dataGridView1.Columns[2], ListSortDirection.Descending);
+            }
+        }
+
+        public void AllTypes(DataGridView dgw)
+        {
+            dgw.Rows.Clear();
+
+            database.OpenConnection();
+
+            string queryString = $"select * from items";
+            NpgsqlCommand comm = new NpgsqlCommand(queryString, database.GetConnection());
+            NpgsqlDataReader read = comm.ExecuteReader();
+
+            while (read.Read())
+            {
+                ReadSingleRow(dgw, read);
+            }
+            read.Close();
+            database.CloseConnection();
+        }
+
+        public void Wearon(DataGridView dgw)
+        {
+            dgw.Rows.Clear();
+
+            database.OpenConnection();
+
+            string queryString = $"select * from items where type_prod LIKE 'Оружие'";
+            NpgsqlCommand comm = new NpgsqlCommand(queryString, database.GetConnection());
+            NpgsqlDataReader read = comm.ExecuteReader();
+
+            while (read.Read())
+            {
+                ReadSingleRow(dgw, read);
+            }
+            read.Close();
+            database.CloseConnection();
+        }
+        private void Decor(DataGridView dgw)
+        {
+            dgw.Rows.Clear();
+            database.OpenConnection();
+
+            string queryString = $"select * from items where type_prod LIKE 'Украшение'";
+            NpgsqlCommand comm = new NpgsqlCommand(queryString, database.GetConnection());
+
+            NpgsqlDataReader read = comm.ExecuteReader();
+
+            while (read.Read())
+            {
+                ReadSingleRow(dgw, read);
+            }
+
+            read.Close();
+            database.CloseConnection();
+        }
+
+        private void MusInst(DataGridView dgw)
+        {
+            dgw.Rows.Clear();
+
+            string queryString = $"select * from items where type_prod LIKE 'Музыкальный инструмент'";
+            NpgsqlCommand comm = new NpgsqlCommand(queryString, database.GetConnection());
+
+            database.OpenConnection();
+
+            NpgsqlDataReader read = comm.ExecuteReader();
+
+            while (read.Read())
+            {
+                ReadSingleRow(dgw, read);
+            }
+
+            read.Close();
+            database.CloseConnection();
+        }
+        private void comboBoxFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxSort.SelectedItem == "Все типы")
+            {
+                AllTypes(dataGridView1);
+            }
+
+            if (comboBoxSort.SelectedItem == "Оружие")
+            {
+                Wearon(dataGridView1);
+            }
+
+            if (comboBoxSort.SelectedItem == "Украшение")
+            {
+                Decor(dataGridView1);
+            }
+
+            if (comboBoxSort.SelectedItem == "Музыкальный инструмент")
+            {
+                MusInst(dataGridView1);
+            }
+        }
+        private void DrawBarcode(string code, int resolution = 20) // resolution - пикселей на миллиметр
+        {
+            int numberCount = 15; // количество цифр
+            float height = 25.93f * resolution; // высота штрих кода
+            float lineHeight = 22.85f * resolution; // высота штриха
+            float leftOffset = 3.63f * resolution; // свободная зона слева
+            float rightOffset = 2.31f * resolution; // свободная зона справа
+                                                    //штрихи, которые образуют правый и левый ограничивающие знаки,
+                                                    //а также центральный ограничивающий знак должны быть удлинены вниз на 1,65мм
+            float longLineHeight = lineHeight + 1.65f * resolution;
+            float fontHeight = 2.75f * resolution; // высота цифр
+            float lineToFontOffset = 0.165f * resolution; // минимальный размер от верхнего края цифр до нижнего края штрихов
+            float lineWidthDelta = 0.15f * resolution; // ширина 0.15*{цифра}
+            float lineWidthFull = 1.35f * resolution; // ширина белой полоски при 0 или 0.15*9
+            float lineOffset = 0.2f * resolution; // между штрихами должно быть расстояние в 0.2мм
+
+            float width = leftOffset + rightOffset + 6 * (lineWidthDelta + lineOffset) + numberCount * (lineWidthFull + lineOffset); // ширина штрих-кода
+
+            Bitmap bitmap = new Bitmap((int)width, (int)height); // создание картинки нужных размеров
+            Graphics g = Graphics.FromImage(bitmap); // создание графики
+
+            Font font = new Font("Arial", fontHeight, FontStyle.Regular, GraphicsUnit.Pixel); // создание шрифта
+
+            StringFormat fontFormat = new StringFormat(); // Центрирование текста
+            fontFormat.Alignment = StringAlignment.Center;
+            fontFormat.LineAlignment = StringAlignment.Center;
+
+            float x = leftOffset; // позиция рисования по x
+            for (int i = 0; i < numberCount; i++)
+            {
+                int number = Convert.ToInt32(code[i].ToString()); // число из кода
+                if (number != 0)
+                {
+                    g.FillRectangle(Brushes.Black, x, 0, number * lineWidthDelta, lineHeight); // рисуем штрих
+                }
+                RectangleF fontRect = new RectangleF(x, lineHeight + lineToFontOffset, lineWidthFull, fontHeight); // рамки для буквы
+                g.DrawString(code[i].ToString(), font, Brushes.Black, fontRect, fontFormat); // рисуем букву
+                x += lineWidthFull + lineOffset; // смещаем позицию рисования по x
+                if (i == 0 && i == numberCount / 2 && i == numberCount - 1) // если это начало, середина или конец кода рисуем разделители
+                {
+                    for (int j = 0; j < 2; j++) // рисуем 2 линии разделителя
+                    {
+                        g.FillRectangle(Brushes.Black, x, 0, lineWidthDelta, longLineHeight); // рисуем длинный штрих
+                        x += lineWidthDelta + lineOffset; // смещаем позицию рисования по x
+                    }
+                }
+            }
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // делаем чтобы картинка помещалась в pictureBox
+            pictureBox1.Image = bitmap; // устанавливаем картинку
+        }
+
+        int selectedRow;
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedRow = e.RowIndex;
+
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[selectedRow];
+
+                textBoxArticle.Text = row.Cells[0].Value.ToString();
+                textBoxName.Text = row.Cells[1].Value.ToString();
+                textBoxCost.Text = row.Cells[2].Value.ToString();
+                textBoxType.Text = row.Cells[3].Value.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawBarcode(textBoxArticle.Text);
+        }
+
+
+        //стрaницы
+        private int currentPage = 1;
+        private int itemsPerPage = 5;
+
+        private void FillDataGridView()
+        {
+            database.OpenConnection();
+
+            int offset = (currentPage - 1) * itemsPerPage;
+
+            string query = $"SELECT * FROM items ORDER BY article OFFSET {offset} ROWS FETCH NEXT {itemsPerPage} ROWS ONLY";
+
+            using (NpgsqlCommand cmd = new NpgsqlCommand(query, database.GetConnection()))
+            {
+                using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(cmd))
+                {
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dataGridView1.DataSource = table;
+                }
+            }
+            database.CloseConnection();
+        }
+    
+        private void button3_Click(object sender, EventArgs e)
+        {
+            database.OpenConnection();
+            string countQuery = "SELECT COUNT(*) FROM items";
+            using (NpgsqlCommand countCmd = new NpgsqlCommand(countQuery, database.GetConnection()))
+            {
+                int totalItems = Convert.ToInt32(countCmd.ExecuteScalar());
+                int totalPages = (int)Math.Ceiling((double)totalItems / itemsPerPage);
+
+                if (currentPage < totalPages)
+                {
+                    currentPage++;
+                    FillDataGridView();
+                }
+            }
+            database.CloseConnection();
+         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+                FillDataGridView();
+            }
+        }
+    }
+}
